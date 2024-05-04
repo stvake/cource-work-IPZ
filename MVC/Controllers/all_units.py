18678bef03c5bb372894c5ea6ef52f144ea7d02a
1raulit <1raulit11122004@gmail.com>
2024-05-04 23:11:50 +0300

    Додано вікно підрозділів

--- MVC/Controllers/all_units.py
+++ MVC/Controllers/all_units.py
@@ -3,3 +3,23 @@ class UnitsController:
         self.model = model
         self.view = view
         self.tab = self.view.tabs['Units']
+        self.tab.sortByCost_Button.config(command=self.sortByCost)
+        self.tab.closeTab_Button.config(command=self.close_tab)
+        self.tab.closeTabWithoutSave_Button.config(command=self.close_tab_without_save)
+        self.tab.openUnitWorkers_Button.config(command=self.open_unit_workers)
+        self.tab.openUnitProjects_Button.config(command=self.open_unit_projects)
+
+    def sortByCost(self):
+        pass
+
+    def close_tab(self):
+        pass
+
+    def close_tab_without_save(self):
+        self.tab.notebook.forget(self.tab.mainFrame)
+
+    def open_unit_workers(self):
+        pass
+
+    def open_unit_projects(self):
+        pass
--- MVC/Views/all_units.py
+++ MVC/Views/all_units.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import tkinter.ttk as ttk
 from MVC.Views.vertical_scrolled_frame import VerticalScrolledFrame
+from MVC.Views.editable_table import EditableTable
 
 
 class AllUnitsView:
@@ -11,6 +12,33 @@ class AllUnitsView:
 
         self.frame = VerticalScrolledFrame(self.mainFrame)
         self.frame.pack(expand=True, fill=BOTH)
+
+        self.sortByCost_Button = ttk.Button(self.frame, text="Відсортувати за сумарною вартістю проектів")
+        self.sortByCost_Button.pack(anchor=E, padx=5, pady=5)
+
+        self.units_table = EditableTable(self.frame, columns=('Name', 'number_of_workers', 'number_of_active_projects',
+                                                              'number_of_all_projects', 'projects_cost'),
+                                                              show='headings', height=10)
+        self.units_table.heading('Name', text='Назва підрозділу')
+        self.units_table.heading('number_of_workers', text='Кількість робітників')
+        self.units_table.heading('number_of_active_projects', text='Кількість не завершених проектів')
+        self.units_table.heading('number_of_all_projects', text='Кількість всіх проектів')
+        self.units_table.heading('projects_cost', text='Сумарна вартість усіх проектів')
+        self.units_table.pack()
+
+        self.openUnitWorkers_Button = ttk.Button(self.frame,
+                                            text="Відкрити список всіх робітників вибраного підрозділу")
+        self.openUnitWorkers_Button.pack(fill=BOTH, padx=5, pady=5)
+
+        self.openUnitProjects_Button = ttk.Button(self.frame,
+                                                  text="Відкрити вкладку всіх проектів вибраного підрозділу")
+        self.openUnitProjects_Button.pack(fill=BOTH, padx=5, pady=5)
+
+        self.closeTab_Button = ttk.Button(self.frame, text="Зберегти та закрити вкладку")
+        self.closeTab_Button.pack(fill=BOTH, padx=5, pady=5)
+
+        self.closeTabWithoutSave_Button = ttk.Button(self.frame, text="Закрити без збереження")
+        self.closeTabWithoutSave_Button.pack(fill=BOTH, padx=5, pady=5)
 
     def add_tab(self):
         self.notebook.insert("end", self.mainFrame, text="Список підрозділів")
