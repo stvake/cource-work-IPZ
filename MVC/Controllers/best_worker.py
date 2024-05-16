dcef7f5f61390f4eac1a684dd37de9b418759905
stvake <stvakemail@gmail.com>
2024-05-16 22:30:07 +0300

    Найкращий працівник має можливість додатися

--- MVC/Controllers/best_worker.py
+++ MVC/Controllers/best_worker.py
@@ -1,3 +1,6 @@
+from MVC.Controllers.worker import WorkerController
+
+
 class BestWorkerController:
     def __init__(self, model, view, post_name):
         self.model = model
@@ -5,10 +8,23 @@ class BestWorkerController:
         self.post_name = post_name
         self.tab = self.view.create_tab('BestWorker', self.post_name)
         self.tab.close_button.config(command=self.close_tab)
+        self.worker_id = None
+        self.worker_controller = None
         self.find_best_worker()
+        self.show_best_worker()
 
     def find_best_worker(self):
-        pass
+        ...
+
+    def show_best_worker(self):
+        self.view.create_tab(
+            'Worker',
+            self.worker_id,
+            self.worker_id,
+            self.tab.worker_frame,
+            self.tab.notebook
+        )
+        self.worker_controller = WorkerController(self, self.model, self.view, self.worker_id)
 
     def close_tab(self):
         self.tab.notebook.forget(self.tab.mainFrame)
--- MVC/Views/best_worker.py
+++ MVC/Views/best_worker.py
@@ -1,4 +1,3 @@
-from tkinter import *
 import tkinter.ttk as ttk
 
 
@@ -8,8 +7,11 @@ class BestWorkerView:
 
         self.mainFrame = ttk.Frame(self.notebook)
 
-        self.notebook.insert("end", self.mainFrame, text="Найкращий працівник на посаді:" + post_name)
+        self.notebook.insert("end", self.mainFrame, text="Найкращий працівник на посаді: " + post_name)
         self.notebook.select(self.mainFrame)
 
+        self.worker_frame = ttk.Frame(self.mainFrame)
+        self.worker_frame.pack(padx=5, pady=5)
+
         self.close_button = ttk.Button(self.mainFrame, text="Закрити вкладку")
-        self.close_button.pack()
+        self.close_button.pack(fill='both', padx=5, pady=5)
